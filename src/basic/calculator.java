c129c2501b1c8036888bcbc0f3f072fc0e574d13
Roohina12 <rummehane@gmail.com>
2022-08-18 16:59:49 +0530

    k

---
+++ src/basic/calculator.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class calculator {
+	System.out.println("Addition");
+
+	        Math.add(22, 67);
+
+	        Math.add(65, 89, 43);
+
+	        System.out.println("Multiplication");
+
+	        Math.multiply(23, 8);
+
+}
